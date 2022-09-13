632258fb6e4d7c644f4dfc123bb8e3a43df08b9c
Taylor Barinka <barinkataylor@gmail.com>
2022-09-13 17:36:03 -0500

    establish basic code to populate home screen with appropriate elements

--- src/home.js
+++ src/home.js
@@ -1,4 +1,6 @@
 import './style.css';
+import Pizza from './pizza.png';
+
 
 function header() {
     const h = document.createElement('h1');
@@ -6,5 +8,14 @@ function header() {
     h.classList.add('header');
     return h;
 }
+const homeImage = new Image();
+homeImage.src = Pizza;
 
-export { header };
+function blurb() {
+    const p = document.createElement('p');
+    p.textContent = "Tony Pizza makes a pie and a slice with that authentic Tony touch.";
+    return p;
+}
+
+
+export { header, homeImage, blurb };
--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
-import { header } from './home.js';
+import { header, homeImage, blurb } from './home.js';
 
 console.log("test");
 const content = document.getElementById("content");
 
 content.appendChild(header());
+content.appendChild(homeImage);
+content.appendChild(blurb());
 
 
