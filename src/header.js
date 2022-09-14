b519a7fa9469ff484c2205c2f3dd1199cb460301
Taylor Barinka <barinkataylor@gmail.com>
2022-09-14 14:02:15 -0500

    style header

--- src/header.js
+++ src/header.js
@@ -5,29 +5,46 @@ import { loadMenu } from './menu.js';
 
 
 function loadHeader() {
+    const header = document.createElement('div');
+        header.classList.add("header");
+    const content = document.getElementById("content");
+    const redStrip = document.createElement('div');
+        redStrip.classList.add('red-strip');
+    const container = document.createElement('div');
+        container.classList.add('header-container');
+    const buttonContainer = document.createElement('div');
+        buttonContainer.classList.add('button-container');
     const homeBtn = document.createElement('button');
         homeBtn.textContent = "Home";
+        homeBtn.classList.add('header-button');
         homeBtn.addEventListener('click', (e) => {
                 loadHome();
         });
     const contactBtn = document.createElement('button');
         contactBtn.textContent = "Contact";
+        contactBtn.classList.add('header-button');
         contactBtn.addEventListener('click', (e) => {
             loadContact();
         });
     const menuBtn = document.createElement('button');
         menuBtn.textContent = "Menu";
+        menuBtn.classList.add('header-button');
         menuBtn.addEventListener('click', (e) => {
         loadMenu();
         });
-    const header = document.createElement('div');
-        header.classList.add("header");
-    const content = document.getElementById("content");
+    const h = document.createElement('h1');
+    h.textContent = "Tony Pizza Slice and Pie"
+    h.classList.add('title');
+
 
     document.body.insertBefore(header, content);
-    header.appendChild(homeBtn);
-    header.appendChild(contactBtn);
-    header.appendChild(menuBtn);
+    header.appendChild(redStrip);
+    header.appendChild(container);
+        container.appendChild(h);
+        container.appendChild(buttonContainer);
+            buttonContainer.appendChild(homeBtn);
+            buttonContainer.appendChild(contactBtn);
+            buttonContainer.appendChild(menuBtn);
 }
 
 export { loadHeader }
--- src/home.js
+++ src/home.js
@@ -8,16 +8,13 @@ homeImage.src = Pizza;
 homeImage.classList.add("image");
 
 function loadHome() {
-    const h = document.createElement('h1');
-    h.textContent = "Tony Pizza"
-    h.classList.add('title');
+
     
     const p = document.createElement('p');
     p.textContent = "Tony Pizza makes a pie and a slice with that authentic Tony touch.";
     
     content.textContent = "";
 
-    content.appendChild(h);
     content.appendChild(homeImage);
     content.appendChild(p);
 }
